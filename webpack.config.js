da36156bafdcb27d02cd59f65078b2be8115abf1
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-07-06 13:43:05 +0100

    Build Created webpack config file with necessary configurations for react-typescript

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	mode: 'development',
+	entry: './src/Index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {minimize: true}
+					}
+				]
+			},
+			{
+				test: /\.tsx?$/,
+				enforce:"pre",
+				loader:'tslint-loader'
+			},
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			},
+
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}, {
+					loader: 'sass-loader'
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}]
+			},
+			{
+				test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				use: 'url-loader?limit=10000',
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts/'
+					}
+				}]
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+						},
+					},
+				],
+			},
+		]
+	},
+	devServer: {
+		historyApiFallback: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: path.resolve(__dirname, 'src', 'index.html'),
+			filename: './index.html'
+		}),
+	]
+};
+
