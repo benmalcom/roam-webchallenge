a653eca6ec29dbfb700cd38ca524b93571a28039
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-07-06 16:25:02 +0100

    Src/graphql/stop.tsx created graphql query to fetch stops from API endpoint

---
+++ src/graphql/stop.tsx
@@ -0,0 +1,29 @@
+export const getStopsQuery = (lat: number, long: number, cursor?: (object | null)) => {
+	const queryCursor = cursor ? `${Object.keys(cursor)[0]}:"${Object.values(cursor)[0]}"` : '';
+	console.log('queryCursor ', queryCursor);
+	console.log('cursor ', cursor);
+	return {
+		query: `
+		  {
+		  stopsByRadius(${queryCursor}, first: 15, lat:${lat}, lon:${long}, radius: 500) {
+			edges {
+			  node {
+				stop {
+				gtfsId
+				name
+				desc
+				}
+			  distance
+			}
+		  }
+		  pageInfo {
+		  hasNextPage
+		  hasPreviousPage
+		  startCursor
+		  endCursor
+		}
+		}
+		}
+	 }`,
+	};
+};
